0790103c84414c32a647ff5613b751f0b5758ff2
z n <quicksandzn@gmail.com>
2019-03-25 14:01:15 +0800

    init

--- src/main/java/elasticsearch/script/SampleScriptPlugin.java
+++ src/main/java/elasticsearch/script/SampleScriptPlugin.java
@@ -4,6 +4,7 @@ package elasticsearch.script;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.plugins.ScriptPlugin;
+import org.elasticsearch.script.ScriptEngineService;
 
 /**
  * Created by zhangshuai@taoche.com on 2019/3/22
@@ -11,7 +12,7 @@ import org.elasticsearch.plugins.ScriptPlugin;
 public class SampleScriptPlugin extends Plugin implements ScriptPlugin {
 
     @Override
-    public org.elasticsearch.script.ScriptEngineService getScriptEngineService(Settings settings) {
+    public ScriptEngineService getScriptEngineService(Settings settings) {
         return new SampleScriptEngineService();
     }
 }
